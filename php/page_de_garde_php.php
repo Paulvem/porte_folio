a270c4421bdba6adecd59c2e7bbffa0a6f8d0b9f
Paul Cardron <paul.cardron@sts-sio-caen.info>
2024-10-31 15:58:25 +0100

    relier php et yaml

--- php/page_de_garde_php.php
+++ php/page_de_garde_php.php
@@ -1,12 +1,13 @@
 <?php
 
 require_once("yaml/yaml.php");
-$data=yaml_parse_file('page_de_garde.yaml');
+$data=yaml_parse_file('../yaml/page_de_garde_yaml.yaml');
 //print_array($data);
 
-echo "<h1>".$data["titre"]."</h1>\n";
+echo "<h1>".$data["Titre"]."</h1>\n";
 echo "<h2>Identite</h2>\n";
 
 foreach($data["Identite"] AS $Identite){
 	echo "<p>".ucfirst($Identite["nom"])." : ".$Identite["Prenom"]." ".$Identite["Age"]."</p>\n";
 }
+?>
