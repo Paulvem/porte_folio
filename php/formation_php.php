2ed4c1c89b91089de823d3f565235fb055bb7a35
Paul Cardron <paul.cardron@sts-sio-caen.info>
2024-10-31 16:58:41 +0100

    Dernier commit

--- php/formation_php.php
+++ php/formation_php.php
@@ -2,3 +2,9 @@
 
 require_once("yaml/yaml.php");
 $data=yaml_parse_file('../yaml/formation_yaml.yaml');
+
+echo "<h1>".$data["Titre"]."</h1>\n";
+
+foreach($data["Formation"] AS $Formation){
+	echo "<p>".ucfirst($Formation["Nom"])." : ".$Formation["Année"]."</p>\n";
+}
